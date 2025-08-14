53dc2d1f82c45a0df734ca093dd819eca7504b57
Moppi <33616102+moppi79@users.noreply.github.com>
2025-08-14 11:52:45 +0200

    Update end_tasker.py
    kommentare eingeführt

--- end_tasker.py
+++ end_tasker.py
@@ -5,12 +5,16 @@ from multiprocessing import shared_memory
 #from multiprocessing import Process
 
 '''
+Das hier ist der end task 
+es wird gestartet wenn der call zustande kommt 
 
+es ist aber nur ein grobes system wurde nicht voll ausgebaut
+
+wie z.b. das er auch bei geht andere nachrichten öndert oder oder oder 
 
 
 '''
 def end_task (time_s:int,target:tuple,own_ip:str,share_name:str,message:bytes)->None:	
-	print ('start',time_s,share_name,message,target,own_ip)
 	
 	#exit('end Tasker put out data')
 	
@@ -19,20 +23,20 @@ def end_task (time_s:int,target:tuple,own_ip:str,share_name:str,message:bytes)->
 	while i > 1:
 		time.sleep(1)
 		i -= 1
-		print (i,shm_b[1])
+		print (i,shm_b[1]) #das soll nachher prüfen ob der A-TLN  aufgelegt hat und sich selbst terminieren
 	
 
 	HOST, PORT = target[0], target[1]
 	
 	# SOCK_DGRAM is the socket type to use for UDP sockets
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	print (dir(sock))
+
 	#exit()
 	a = True
 	while a:
 		i = random.randrange(15000,15999)
 		#i = 15223
-		print (i)
+
 		try: 
 			sock.bind((own_ip,i))
 		
@@ -40,18 +44,14 @@ def end_task (time_s:int,target:tuple,own_ip:str,share_name:str,message:bytes)->
 			pass
 		finally:
 			a  = False
-			print(own_ip,i)
+
 		
 
 		time.sleep(0.02)
 	
-	print(dir(sock))		
+
 	# As you can see, there is no connect() call; UDP has no connections.
 	# Instead, data is directly sent to the recipient via sendto().
 	sock.sendto(message, (HOST, PORT))
 
-	print("Sent:   ",HOST,PORT, message)
 	sock.close()
-	
-	print ('ende')
-	#shm_b.shm.unlink()
